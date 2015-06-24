2997c4749dff211c5e9937b1d2036d528dd625de
KolevDarko <kolevdarko@gmail.com>
2015-06-24 07:46:17 +0200

    Implemented orders tax calculator

---
+++ Orders.exs
@@ -0,0 +1,30 @@
+
+defmodule Orders do
+	# Calculate tax rates for given array of orders and given array of tax rates for
+	# specific states, the other states have no tax
+	def calcTax do
+		tax_rates = [ NC: 0.075, TX: 0.08 ]
+
+		orders = [   
+		    [ id: 123, ship_to: :NC, net_amount: 100.00 ],
+		    [ id: 124, ship_to: :OK, net_amount:  35.50 ],
+		    [ id: 125, ship_to: :TX, net_amount:  24.00 ],
+		    [ id: 126, ship_to: :TX, net_amount:  44.80 ],
+		    [ id: 127, ship_to: :NC, net_amount:  25.00 ],
+		    [ id: 128, ship_to: :MA, net_amount:  10.00 ],
+		    [ id: 129, ship_to: :CA, net_amount: 102.00 ],
+		    [ id: 120, ship_to: :NC, net_amount:  50.00 ] ]
+
+		rate_ids = for {r, v} <- tax_rates, into: HashSet.new, do: r
+
+		tax_orders = for order <- orders do
+						 if Set.member?(rate_ids, order[:ship_to]) do
+						 	value = tax_rates[order[:ship_to]]
+							Enum.into(order, [{:tax_amount, (order[:net_amount]*(1+value))}])
+						else
+							Enum.into(order, [{:tax_amount, order[:net_amount]}])
+						end
+					end
+		tax_orders
+	end
+end
